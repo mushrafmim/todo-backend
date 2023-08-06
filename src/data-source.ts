368f0b72f2f7d3fe5e6299225826a287a3a4bd2b
Mohamed Mushraf <mushrafmim@gmail.com>
2023-08-06 22:15:37 +0530

    Done.

--- src/data-source.ts
+++ src/data-source.ts
@@ -2,14 +2,15 @@ import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { User } from "./entity/User"
 import { ToDo } from "./entity/ToDo"
+import { config } from "dotenv"
+
+config()
+
+const db_uri = process.env.DB_URI
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "root",
-    password: "Mm008911-*",
-    database: "todo",
+    url: db_uri,
     synchronize: true,
     logging: false,
     entities: [User, ToDo],
